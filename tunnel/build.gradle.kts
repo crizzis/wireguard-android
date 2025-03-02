a6e14f3d0c9d58417a8cdfea748496703f65bbf2
Krzysztof Sierszeń <krzysztof.sierszen@gmail.com>
2025-03-02 19:41:58 +0100

    Version bump + config

--- tunnel/build.gradle.kts
+++ tunnel/build.gradle.kts
@@ -110,19 +110,4 @@ publishing {
             }
         }
     }
-    repositories {
-        maven {
-            name = "sonatype"
-            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = providers.environmentVariable("SONATYPE_USER").orNull
-                password = providers.environmentVariable("SONATYPE_PASSWORD").orNull
-            }
-        }
-    }
-}
-
-signing {
-    useGpgCmd()
-    sign(publishing.publications)
 }
